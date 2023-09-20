c3f8f86ae3e7b363b0136e410b58284745e12fc6
Hector Bjernersjo <hector@bjernersjo.se>
2023-09-20 18:54:47 +0200

    aaaa

--- actual_project/connect_to_server.py
+++ actual_project/connect_to_server.py
@@ -3,15 +3,18 @@ import requests
 import time
 
 cap = cv2.VideoCapture(0)
-VIDEO_PATH = "http://192.168.50.205:5000/process_frame"
-TEST_PATH = "http://192.168.50.205:5000/test"
+# VIDEO_PATH = "http://192.168.50.205:5000/process_frame"
+# TEST_PATH = "http://192.168.50.205:5000/test"
+
+VIDEO_PATH = "http://192.168.50.242:5000/process_frame"
+TEST_PATH = "http://192.168.50.242:5000/test"
 
 frame_times = []
 
 while True:
     start_time = time.time()
-    ret, frame = cap.read()
-    _, img_encoded = cv2.imencode('.jpg', frame)
+    # ret, frame = cap.read()
+    # _, img_encoded = cv2.imencode('.jpg', frame)
     # response = requests.post(VIDEO_PATH, files={'frame': img_encoded.tostring()})
     response = requests.get(TEST_PATH)
     # print(response.json())
